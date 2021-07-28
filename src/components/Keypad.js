dc386c6c7a0343607e0720dca0de72cba48094e8
DKay-code <derya.kaygisiz@outlook.com>
2021-07-28 17:17:24 +0300

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad() {
+    function change() {
+        console.log('Entering password...');
+    }
+  
+    return (
+      <div>
+        <input onChange= {change} type="password" />
+      </div>
+    );
+  } 
+  
+  export default Keypad;
